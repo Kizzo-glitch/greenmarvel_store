34e8905163cf6e70b48d297d742019d45d2fcdce
Kizzo-glitch <kizzothwa@gmail.com>
2024-07-20 01:21:28 +0200

    added password environ

--- marvel/settings.py
+++ marvel/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.['DB_PASSWORD'],
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '58664',
     }
