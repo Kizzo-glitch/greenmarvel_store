07dbe5c6f272c31736316a0006161457512a5669
Kizzo-glitch <kizzothwa@gmail.com>
2024-07-20 01:54:47 +0200

    remove dotenv

--- marvel/settings.py
+++ marvel/settings.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-load_dotenv()
+#load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
