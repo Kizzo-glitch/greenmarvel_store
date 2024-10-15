defe3335dfa507dce29e4a80c101837f963c2f6f
Kizzo-glitch <kizzothwa@gmail.com>
2024-10-15 09:35:52 +0200

    adding relevent domains

--- marvel/settings.py
+++ marvel/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 #DEBUG_PROPAGATE_EXCEPTIONS = True
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = [ '127.0.0.1','greenmarvelstore-production.up.railway.app', 'https://greenmarvelstore-production.up.railway.app']
+ALLOWED_HOSTS = [ '127.0.0.1','https://greenmarvel.co.za','greenmarvel.co.za','greenmarvelstore-production.up.railway.app', 'https://greenmarvelstore-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://greenmarvelstore-production.up.railway.app']
 
 #'127.0.0.1',
