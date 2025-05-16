6ae3cee793443ca6cbcbeedf29d83412428ee943
Khotso Serabele <serabelekd@gmail.com>
2025-05-16 18:55:28 +0200

    making products page index

--- greenmarv/urls.py
+++ greenmarv/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('index/', views.index, name='index'),   
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
